80ef1441efa1e7b7c1f2c70e8c45c193b3e55aa4
Eric Rivera <ericrivera@Eric-Riveras-MacBook-Pro.local>
2018-06-22 15:16:41 -0700

    chill cat progress

--- src/App.js
+++ src/App.js
@@ -2,9 +2,63 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super(); 
+      this.state = {
+      picture: 'thispic',
+      name: 'rexx',
+      friends: [{
+        name: 'My 404 friend',
+        picture: 'https://http.cat/404'
+      }, {
+        name:'My cat with the muchies',
+        picture: 'https://http.cat/420'
+      }]
+    };
+  }
+    changeName (event){
+      this.setState({
+        name: event.target.value
+      })
+      
+    }
+    newPicture (picture){
+      this.setState({
+        picture: picture.target.value
+      })
+    }
+
+    addFriend(){
+      const newFriend = {
+        name: this.state.name,
+        picture: this.state.picture,
+      };
+      this.setState({
+        friends: this.state.friends.concat(newFriend)
+      });
+    }
+
   render() {
     return (
-      <div>sup eric</div>
+      <div>
+        Name: <input value={this.state.name} onChange={(event) => this.changeName(event)} />
+       
+        picture: <input value={this.state.picture} onChange ={(event) => this.newPicture(event)} />
+
+        <button onClick={() => this.addFriend()}>Add friend</button>
+
+        <div> 
+          {this.state.friends.map(friend => {
+            console.log('friends', friend);
+            return (
+              <div>
+                <p><img src = {friend.picture} alt= {friend.name}/> state.name</p>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+
     );
   }
 }
